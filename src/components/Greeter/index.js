133f2a9f9767bfe3f060cd50ee0a23f4acd2979b
hendra sadewa <hendra.sadewa@hara.ag>
2019-10-14 23:39:02 +0700

    greeter turned into functional component

--- src/components/Greeter/index.js
+++ src/components/Greeter/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class Greeter extends React.Component {
-  render() {
-    return <h1>Hi {this.props.name || 'anon'}</h1>;
-  }
+function Greeter(props) {
+  return <h1>Hi {props.name || 'anon'}</h1>;
 }
 
 export default Greeter;
